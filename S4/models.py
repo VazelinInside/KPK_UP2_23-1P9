9af86e9293c483e6c79e685a7a006c0979138cd8
VazelinInside <vi4733425@gmail.com>
2026-05-18 14:14:34 +0300

    2.0

--- S4/models.py
+++ S4/models.py
@@ -1,18 +1,27 @@
-from peewee import SqliteDatabase, Model, CharField
+from peewee import SqliteDatabase, Model, CharField, ForeignKeyField, IntegerField
 
 db = SqliteDatabase('permissions.db')
 
 class Permission(Model):
-    name = CharField(max_length=100, unique=True, null=False, verbose_name="Название разрешения")
-    resource = CharField(max_length=100, null=False, verbose_name="Ресурс")
-    action = CharField(max_length=50, null=False, verbose_name="Действие")
-    description = CharField(max_length=255, null=False, default='', verbose_name="Описание")
+    name = CharField(max_length=100, unique=True, null=False)
+    description = CharField(max_length=255, null=False, default='')
 
     class Meta:
         database = db
-        table_name = 'permissions' 
+        table_name = 'permissions'
+
+class RolePermission(Model):
+    role_id = IntegerField(null=False)
+    permission_id = ForeignKeyField(Permission, backref='roles')
+
+    class Meta:
+        database = db
+        table_name = 'role_permissions'
+        indexes = (
+            (('role_id', 'permission_id'), True),
+        )
 
 def init_db():
     db.connect()
-    db.create_tables([Permission], safe=True)
+    db.create_tables([Permission, RolePermission], safe=True)
     db.close()
